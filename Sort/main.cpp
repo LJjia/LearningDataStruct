5e812bbe860c993c7d3e9e608b32abd2c42ae2ab
ljjiahf <ljjiahf@163.com>
2021-10-16 22:46:39 +0800

    ubuntu 提交改动,主要是快速排序优化

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -7,12 +7,14 @@ void TestInsertSort();
 //void TestShellSort();
 //void TestHeapSort();
 void TestMergeSort();
+void TestQuickSort();
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 //    TestBubble();
 //    TestSelect();
 //    TestInsertSort();
-    TestMergeSort();
+//    TestMergeSort();
+    TestQuickSort();
     return 0;
 }
